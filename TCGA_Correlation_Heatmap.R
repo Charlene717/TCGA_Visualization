85cea8ea97fbdffbe1b3857afd06fa133ba97cef
Charlene <q2330416@gmail.com>
2023-02-20 19:51:07 +0800

    Packages Setting

--- TCGA_Correlation_Heatmap.R
+++ TCGA_Correlation_Heatmap.R
@@ -4,10 +4,14 @@ memory.limit(150000)
 
 ##### Load Packages #####
 source("FUN_Package_InstLoad.R")
-PKG_Basic.set <- c("tidyverse")
-PKG_BiocManager.set <- c("clusterProfiler","enrichplot","pathview","limma")
+PKG_Basic.set <- c("tidyverse","circlize")
+PKG_BiocManager.set <- c("ComplexHeatmap")
 
 FUN_Package_InstLoad(Basic.set = PKG_Basic.set, BiocManager.set = PKG_BiocManager.set)
+
+##### Function setting #####
+source("FUN_Beautify_ggplot.R")
+source("FUN_ggPlot_vline.R")
 
 
 ##### Import setting* #####
